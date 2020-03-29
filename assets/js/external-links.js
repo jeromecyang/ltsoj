130a574f10235becfed3e4953b574007b141300b
Jerome Yang <cryang1018@gmail.com>
2020-03-29 14:16:14 -0400

    Fix external links and update analytics

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
   document.querySelectorAll('a[href]').forEach(e => {
-    if (e.getAttribute('href').match(/^http/g)) {
+    if (e.getAttribute('href').match(/^http/g) && !e.getAttribute('href').match(/^ltsoj\.com/g)) {
       e.setAttribute('target', '_blank')
     }
   });
