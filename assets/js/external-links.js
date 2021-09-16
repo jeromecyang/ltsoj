2369bdd52f9bd4174e523519b69b72c322fd5006
Jerome Yang <cryang1018@gmail.com>
2021-09-15 22:20:45 -0700

    Improvement

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,7 +1,8 @@
 setTimeout(() => {
   document.querySelectorAll('a[href]').forEach(e => {
     if (e.getAttribute('href').match(/^http/g) && !e.getAttribute('href').match(/^ltsoj\.com/g)) {
-      e.setAttribute('target', '_blank')
+      e.setAttribute('target', '_blank');
+      e.setAttribute('rel', 'noopener');
     }
   });
 }, 2000);
